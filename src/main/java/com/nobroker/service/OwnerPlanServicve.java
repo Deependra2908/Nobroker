336f861001c883384e13205cedb931a3a8bee614
deependra2908 <deependrasahu34@gmail.com>
2024-02-26 16:08:19 +0530

    5th jan 2024

--- src/main/java/com/nobroker/service/OwnerPlanServicve.java
+++ src/main/java/com/nobroker/service/OwnerPlanServicve.java
@@ -2,6 +2,10 @@ package com.nobroker.service;
 
 import com.nobroker.payload.OwnerPlanDto;
 
+import java.util.List;
+
 public interface OwnerPlanServicve {
     public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
+
+    List<OwnerPlanDto> getAllOwnerPlans();
 }
---
+++ src/main/java/com/nobroker/service/UserService.java
@@ -0,0 +1,17 @@
+package com.nobroker.service;
+
+import com.nobroker.entity.User;
+import com.nobroker.payload.UserDto;
+
+public interface UserService {
+
+    User getUserByEmail(String email);
+
+    public long createUser(UserDto userDto);
+
+    public User registerUser(User user);
+
+   public void verifyEmail(User user);
+
+
+}
