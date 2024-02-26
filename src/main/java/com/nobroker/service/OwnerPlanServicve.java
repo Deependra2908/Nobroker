184ca293f4dbfb7b79b3afb080b9d5105311c3b9
deependra2908 <deependrasahu34@gmail.com>
2024-02-26 16:08:19 +0530

    5th jan 2024

---
+++ src/main/java/com/nobroker/service/OwnerPlanServicve.java
@@ -0,0 +1,7 @@
+package com.nobroker.service;
+
+import com.nobroker.payload.OwnerPlanDto;
+
+public interface OwnerPlanServicve {
+    public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
+}
