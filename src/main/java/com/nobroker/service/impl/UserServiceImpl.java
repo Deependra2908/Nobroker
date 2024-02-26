7c95fe7d79a6d73921b1e6983327670c53aa659d
deependra2908 <deependrasahu34@gmail.com>
2024-02-26 16:08:19 +0530

    5th jan 2024

---
+++ src/main/java/com/nobroker/service/impl/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.nobroker.service.impl;
+
+import com.nobroker.entity.User;
+import com.nobroker.payload.UserDto;
+import com.nobroker.repository.UserRepository;
+import com.nobroker.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+   private ModelMapper modelMapper;
+    @Autowired
+   private UserRepository userRepository;
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public long createUser(UserDto userDto) {
+        User user = mapToEntity(userDto);
+        User save = userRepository.save(user);
+        return save.getId();
+    }
+
+    @Override
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void verifyEmail(User user) {
+        user.setEmailVerified(true);
+        userRepository.save(user);
+    }
+
+    @Override
+    public boolean isEmailVerified(String email) {
+        User user = userRepository.findByEmail(email);
+        return user !=null && user.isEmailVerified() ;
+    }
+
+
+    User mapToEntity(UserDto userDto){
+        User user = modelMapper.map(userDto, User.class);
+        return user;
+    }
+}
